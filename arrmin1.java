926979ddf81cee4a1c1ac1cb059cf0571f524f82
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-09 11:36:39 +0530

    Create arrmin1.java

---
+++ arrmin1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+			
+		}
+	           int min=a[0];
+		for(int i=1;i<n;i++)
+		{
+			if(min>a[i])
+			min=a[i];
+		}
+		System.out.println(min);
+	}
+}
